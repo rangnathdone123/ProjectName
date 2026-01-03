1dad85e1e7830d81c40578cebff07cbf62ca277d
Rangnath Done <rangnathdone@gmail.com>
2026-01-03 13:47:05 +0530

    Fix(engine-control): Add API documentation and add gradual speed ramp-up
    - Add detailed function headers with description, parameters, and return types in EngineControl.h
    - Update EngineControl_Update() to implement gradual speed ramp-up and ramp-down logic

--- EngineControlModule/EngineControl.c
+++ EngineControlModule/EngineControl.c
@@ -1,17 +1,14 @@
 #include "EngineControl.h"
 
-/* Current engine state variable */
 static EngineState engine_state = ENGINE_OFF;
-
-/* Current engine speed in RPM */
 static uint16_t current_speed = ENGINE_SPEED_MIN_RPM;
-
-/* Requested target speed in RPM */
 static uint16_t target_speed = ENGINE_SPEED_MIN_RPM;
+
+/* Increment used for gradual speed ramp-up (RPM per update cycle) */
+#define SPEED_INCREMENT_STEP   (100U)
 
 void EngineControl_Init(void)
 {
-    /* Initialize engine to OFF state and zero speed */
     engine_state = ENGINE_OFF;
     current_speed = ENGINE_SPEED_MIN_RPM;
     target_speed = ENGINE_SPEED_MIN_RPM;
@@ -19,7 +16,6 @@ void EngineControl_Init(void)
 
 void EngineControl_SetSpeed(uint16_t speed)
 {
-    /* Clamp the requested speed to allowable limits */
     if (speed > ENGINE_SPEED_MAX_RPM)
     {
         target_speed = ENGINE_SPEED_MAX_RPM;
@@ -36,28 +32,54 @@ void EngineControl_SetSpeed(uint16_t speed)
 
 uint16_t EngineControl_GetSpeed(void)
 {
-    /* Return current engine speed */
     return current_speed;
 }
 
 EngineState EngineControl_GetState(void)
 {
-    /* Return current engine state */
     return engine_state;
 }
 
 void EngineControl_Update(void)
 {
-    /* If target speed is zero, turn engine OFF */
     if (target_speed == ENGINE_SPEED_MIN_RPM)
     {
+        /* Target speed zero, shut down engine */
         engine_state = ENGINE_OFF;
         current_speed = ENGINE_SPEED_MIN_RPM;
     }
     else
     {
-        /* Otherwise, turn engine ON and set speed to target */
+        /* Engine ON */
         engine_state = ENGINE_ON;
-        current_speed = target_speed;
+
+        if (current_speed < target_speed)
+        {
+            /* Gradually increase speed up to target */
+            uint32_t temp_speed;
+            temp_speed = (uint32_t)current_speed + SPEED_INCREMENT_STEP;
+
+            if (temp_speed > target_speed)
+            {
+                current_speed = target_speed;
+            }
+            else
+            {
+                current_speed = (uint16_t)temp_speed;
+            }
+        }
+        else if (current_speed > target_speed)
+        {
+            /* Gradually decrease speed down to target */
+            if ((current_speed - SPEED_INCREMENT_STEP) < target_speed)
+            {
+                current_speed = target_speed;
+            }
+            else
+            {
+                current_speed -= SPEED_INCREMENT_STEP;
+            }
+        }
+        /* else current_speed == target_speed, do nothing */
     }
 }
